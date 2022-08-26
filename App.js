6599d1747a2a7adce2680e865d4e5926b77f3b7d
ohuji <juhsalomaki@gmail.com>
2022-08-26 14:23:25 +0300

    styling exercise

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Platform} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  View,
+  ImageBackground,
+  Text,
+} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import List from './components/List';
+// import {Settings} from 'react-native-feather';
 
 const App = () => {
   return (
     <>
       <SafeAreaView style={styles.container}>
+        <View style={styles.catLogoView}>
+          <ImageBackground
+            source={require('../MyApp/assets/kisa.jpg')}
+            resizeMode="cover"
+            style={styles.catLogo}
+          >
+            <Text style={styles.logoText}>Homeless kittens</Text>
+            {/* <Settings
+              stroke="white"
+              fill="none"
+              strokeWidth="1"
+              width={40}
+              height={40}
+              style={styles.settings}
+            />*/}
+          </ImageBackground>
+        </View>
         <List />
       </SafeAreaView>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor="#14A76C" />
     </>
   );
 };
@@ -17,8 +42,29 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#272727',
     paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+  catLogo: {
+    height: 320,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginBottom: 10,
+    margin: 5,
+    borderBottomRightRadius: 100,
+    overflow: 'hidden',
+  },
+  logoText: {
+    color: 'white',
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+    padding: 10,
+    marginBottom: 25,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
   },
 });
 
--- components/ListItem.js
+++ components/ListItem.js
@@ -5,15 +5,17 @@ import PropTypes from 'prop-types';
 const ListItem = (props) => {
   return (
     <TouchableOpacity style={styles.touchableOpacity}>
-      <View style={styles.view}>
-        <Image
-          style={styles.image}
-          source={{uri: props.singleMedia.thumbnails.w160}}
-        />
-      </View>
-      <View style={styles.view}>
-        <Text style={styles.text}>{props.singleMedia.title}</Text>
-        <Text>{props.singleMedia.description}</Text>
+      <View style={styles.parentView}>
+        <View style={styles.view}>
+          <Image
+            style={styles.image}
+            source={{uri: props.singleMedia.thumbnails.w160}}
+          />
+        </View>
+        <View style={styles.view}>
+          <Text style={styles.title}>{props.singleMedia.title}</Text>
+          <Text style={styles.desc}> {props.singleMedia.description}</Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -22,19 +24,30 @@ const ListItem = (props) => {
 const styles = StyleSheet.create({
   touchableOpacity: {
     margin: 10,
-    flexDirection: 'row',
   },
   view: {
     flex: 1,
-    backgroundColor: '#d0d0d0',
+    backgroundColor: '#14A76C',
     padding: 5,
+    height: 180,
+    overflow: 'hidden',
   },
   image: {
     flex: 1,
+    borderBottomLeftRadius: 40,
   },
-  text: {
+  title: {
     fontSize: 15,
     fontWeight: 'bold',
+    color: 'white',
+  },
+  desc: {
+    color: 'white',
+    fontSize: 12,
+  },
+  parentView: {
+    backgroundColor: '#14A76C',
+    flexDirection: 'row',
   },
 });
 
